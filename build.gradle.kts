6186e815ae3eae2c2a563c4afc7cc3f3180f4b06
diegolopezgrisanti <diegolopezgrisanti@gmail.com>
2024-10-22 09:26:32 -0700

    add SonarLint fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,19 +18,25 @@ repositories {
 }
 
 dependencies {
+	val FLYWAY = "9.11.0"
+	val LOMBOK = "1.18.34"
+	val POSTGRESQL = "42.7.4"
+
+
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.postgresql:postgresql:42.7.4")
+	implementation("org.postgresql:postgresql:$POSTGRESQL")
+	implementation("org.flywaydb:flyway-core:$FLYWAY")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	// Lombok
-	compileOnly("org.projectlombok:lombok:1.18.34")
-	annotationProcessor("org.projectlombok:lombok:1.18.34")
+	compileOnly("org.projectlombok:lombok:$LOMBOK")
 
-	// Flyway
-	implementation("org.flywaydb:flyway-core:9.11.0")
+	annotationProcessor("org.projectlombok:lombok:$LOMBOK")
 }
 
 tasks.withType<Test> {
